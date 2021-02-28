88834380ab24de37c9c7f694c161c78426fb3a6a
mtbishop <bishopmatthew76@gmail.com>
2021-02-27 20:32:13 -0600

    added link to new notes to json db

--- routing/apirouts.js
+++ routing/apirouts.js
@@ -2,16 +2,29 @@ const fs = require('fs');
 
 module.exports = (app) => {
   app.get('/api/notes', (req, res) => {
-    fs.readFile('.db/db.json', (err, data) => {
+    fs.readFile('./db/db.json', (err, data) => {
       if (err) throw err;
-      notesList = JSON.parse(data);
-      res.send(notesList);
+      noteList = JSON.parse(data);
+      res.send(noteList);
     });
   });
   app.post('/api/notes', (req, res) => {
-      const userNotes = req.body;
+    const userNotes = req.body;
 
-      
-  })
+    fs.readFile('./db/db.json', (err, data) => {
+      if (err) throw err;
+      noteList = JSON.parse(data);
+      noteList.push(userNotes);
+      notesList.forEach((item, x) => {
+        item.id = x + 1;
+        return noteList;
+      });
+      console.log(noteList);
 
+      fs.writeFile('./db/db.json', JSON.stringify(noteList), (err, data) => {
+        if (err) throw err;
+      });
+    });
+    res.status(200).send();
+  });
 };
